708764b3d06481509815d1acb054ffc59314fd77
Black.C <154367712@qq.com>
2017-07-26 17:22:43 +0800

    1.

---
+++ app/src/main/java/gank/hyx/com/gank/main/MainActivity.java
@@ -0,0 +1,15 @@
+package gank.hyx.com.gank.main;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import gank.hyx.com.gank.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/gank/hyx/com/gank/main/MainPresenter.java
@@ -0,0 +1,8 @@
+package gank.hyx.com.gank.main;
+
+/**
+ * Created by Black.C on 2017/7/25.
+ */
+
+public class MainPresenter {
+}
